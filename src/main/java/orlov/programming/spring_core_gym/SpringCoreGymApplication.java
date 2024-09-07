51df7e437cfc30c374745d26def8c62db96dd0b0
OleksandrOrlovIT <oleksandrorlovit@gmail.com>
2024-09-07 12:30:14 +0300

    Init commit

---
+++ src/main/java/orlov/programming/spring_core_gym/SpringCoreGymApplication.java
@@ -0,0 +1,13 @@
+package orlov.programming.spring_core_gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreGymApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreGymApplication.class, args);
+    }
+
+}
---
+++ src/test/java/orlov/programming/spring_core_gym/SpringCoreGymApplicationTests.java
@@ -0,0 +1,13 @@
+package orlov.programming.spring_core_gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreGymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
