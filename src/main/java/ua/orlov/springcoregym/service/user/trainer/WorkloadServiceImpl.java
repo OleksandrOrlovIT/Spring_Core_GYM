dd29ddc139a10f299fc04ef4f9ca10e07b430211
OleksandrOrlovIT <oleksandrorlovit@gmail.com>
2024-11-21 20:54:22 +0200

    Fixed configuration and created ability to call microservice. #33

--- src/main/java/ua/orlov/springcoregym/service/user/trainer/WorkloadServiceImpl.java
+++ src/main/java/ua/orlov/springcoregym/service/user/trainer/WorkloadServiceImpl.java
@@ -1,14 +1,16 @@
 package ua.orlov.springcoregym.service.user.trainer;
 
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.EurekaClient;
-import com.netflix.discovery.shared.Application;
-import com.netflix.eureka.EurekaServerContextHolder;
-import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import ua.orlov.springcoregym.dto.trainer.TrainerWorkload;
 
 @Log4j2
@@ -16,33 +18,42 @@ import ua.orlov.springcoregym.dto.trainer.TrainerWorkload;
 @AllArgsConstructor
 public class WorkloadServiceImpl implements WorkloadService {
 
-    private final RestTemplate restTemplate;
+    private final CloseableHttpClient httpClient;
 
-    private EurekaClient discoveryClient;
+    private final DiscoveryClient discoveryClient;
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public String changeWorkload(TrainerWorkload trainerWorkload) {
-        PeerAwareInstanceRegistry registry = EurekaServerContextHolder.getInstance().getServerContext().getRegistry();
-        String url = "";
+        var instances = discoveryClient.getInstances("GYM-TRAINER-WORKLOAD");
 
-        for (Application registeredApplication :  registry.getApplications().getRegisteredApplications()) {
-            for (InstanceInfo instance : registeredApplication.getInstances()) {
-                if ("GYM-TRAINER-WORKLOAD".equals(instance.getAppName())) {
-                    String host = instance.getHostName();
-                    String id = instance.getInstanceId();
-                    int port = instance.getPort();
-                    url = String.format("http://%s:%s:%d/api/v1/trainer/workload", id, host, port);
-                    break;
-                }
+        if (instances.isEmpty()) {
+            throw new RuntimeException("No instance of GYM-TRAINER-WORKLOAD found in Eureka registry");
+        }
+
+        var instance = instances.get(0);
+        String instanceInfo = instance.toString();
+        String foundStringForIpAddr = "ipAddr = ";
+        int ipAddrIndex = instanceInfo.indexOf(foundStringForIpAddr) + foundStringForIpAddr.length();
+        String ipAddr = instanceInfo.substring(ipAddrIndex, ipAddrIndex + 10);
+
+        String url = String.format("https://%s:%d/api/v1/trainer/workload", ipAddr, instance.getPort());
+        log.info("Calling URL: " + url);
+
+        try {
+            String json = objectMapper.writeValueAsString(trainerWorkload);
+            StringEntity entity = new StringEntity(json, ContentType.APPLICATION_JSON);
+
+            HttpPost post = new HttpPost(url);
+            post.setEntity(entity);
+
+            try (CloseableHttpResponse response = httpClient.execute(post)) {
+                return EntityUtils.toString(response.getEntity());
             }
+        } catch (Exception e) {
+            log.error(e);
+            return "Exception" + e.getMessage();
         }
-
-        if (url.isEmpty()) {
-            throw new RuntimeException("No instance of GYM_TRAINER_WORKLOAD found in Eureka registry");
-        }
-
-        log.error("Calling URL: " + url);
-
-        return restTemplate.postForObject(url, trainerWorkload, String.class);
     }
 }
